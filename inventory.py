01537ede41334d2f694363219dd6b6ce891baa6f
?ngel ?lvarez <angel@nan-tic.com>
2019-03-07 12:52:44 +0100

    fix search for category

--- inventory.py
+++ inventory.py
@@ -41,7 +41,8 @@ class Inventory(metaclass=PoolMeta):
                         ('parent', 'child_of',
                             [inventory.product_category.id]),
                         ])
-                products = Product.search([('category', 'in', categories)])
+                products = Product.search([('categories.id', 'in', [
+                    x.id for x in categories])])
                 product_ids = [p.id for p in products]
             if not product_ids:
                 continue
