79e20ed86fb83e585885e490c0cc17a1801aa9f2
Raimon Esteve <raimon@nan-tic.com>
2019-06-05 23:59:01 +0200

    pyflakes

--- shipment.py
+++ shipment.py
@@ -1,6 +1,6 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
 from trytond.transaction import Transaction
 
 __all__ = ['ShipmentIn', 'ShipmentOut', 'ShipmentOutReturn']
